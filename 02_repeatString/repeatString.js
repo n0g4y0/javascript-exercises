ccf2646348fb81e9b73416a3a119d60baf807f7d
n0g4y0 <42777385+n0g4y0@users.noreply.github.com>
2024-01-30 14:29:08 -0400

    02 repeatString: solved and enabled more use cases in test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(str,n) {
+    if(n >= 0)
+        return str.repeat(n);
+    else    
+        return 'ERROR';
 };
 
 // Do not edit below this line
