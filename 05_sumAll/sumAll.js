bbad36f4a158809d4d48ba18df3fd2dd2e841e1f
n0g4y0 <42777385+n0g4y0@users.noreply.github.com>
2024-01-31 16:51:43 -0400

    problem 05 solved and add more test cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if(typeof a !== 'number' || typeof b  !== 'number' || a < 0 || b < 0) return 'ERROR';
+    let max = Math.max(a,b);
+    let min = Math.min(a,b);
+    let res = 0;
+    for(let i = min; i<=max; i++){
+        res +=i;
+    }
 
+    return res;
 };
 
 // Do not edit below this line
