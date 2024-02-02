4c7da400c2a7088810944ec83d9e1d5f328fcaca
n0g4y0 <42777385+n0g4y0@users.noreply.github.com>
2024-02-01 20:07:22 -0400

    06 leap year problem solved: adding more test cases

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //return year % 4 === 0 || year % 400 ;
+    if(year % 100 === 0){
+        return ((year % 100 === 0 ) && (year % 400 === 0 ) && (year % 4 === 0 )); 
+    }else{
+        return year % 4 === 0;
+    }
+    
 };
 
 // Do not edit below this line
