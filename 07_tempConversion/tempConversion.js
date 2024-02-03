c83a6095aead85320c9cb66d4a49533d1d4d53bc
n0g4y0 <42777385+n0g4y0@users.noreply.github.com>
2024-02-02 20:58:21 -0400

    07 temperature conversion problem solved: adding more test cases

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(t) {
+  let celsius = (5/9)*(t - 32)
+  return  celsius % 1 === 0? celsius: +celsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(t) {
+  let fa = (1.8*t) + 32;
+  return  fa % 1 === 0? fa: +fa.toFixed(1);
 };
 
 // Do not edit below this line
