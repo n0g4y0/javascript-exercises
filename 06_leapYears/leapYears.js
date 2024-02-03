21884c4c22d3481e795bf6264e6a2409492b6e88
n0g4y0 <42777385+n0g4y0@users.noreply.github.com>
2024-02-02 21:00:05 -0400

    06 leap years problem: issue solved

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,12 +1,6 @@
-const leapYears = function(year) {
-    //return year % 4 === 0 || year % 400 ;
-    if(year % 100 === 0){
-        return ((year % 100 === 0 ) && (year % 400 === 0 ) && (year % 4 === 0 )); 
-    }else{
-        return year % 4 === 0;
-    }
-    
-};
+const leapYears = function (year) {
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+  };
 
 // Do not edit below this line
 module.exports = leapYears;
